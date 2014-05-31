fe798dcf4fdb361d500c4332326a193ae2cd88a4
vladmic <vladmic@ymail.com>
2014-05-31 04:24:58 +0300

    Add rating

--- js/script.js
+++ js/script.js
@@ -36,10 +36,25 @@ function gamesController($scope, $http) {
     var aux = [];
 	var aux2 = [];
 	var k =0;
+    var rating = [];
+	var pop = 0;
 	$http.get('assets/data.json')
        .then(function(res){
            for(var i=0;i<8;i++){
              aux[i] = res.data.games[i];  
+			 pop = res.data.games[i].popularity;
+			 if(pop == 0)
+				rating[i] = 0;
+			  else if(pop < 50000)
+			    rating[i] = 1;
+			  else if(pop < 100000)
+			    rating[i] = 2;
+			  else if(pop < 150000)
+			    rating[i] = 3;
+			  else if(pop < 200000)
+			    rating[i] = 4;
+			  else if(pop > 200000)
+			    rating[i] = 5;
            		  }
 		   for(var j=i;j<17;j++){
 				 aux2[k] = res.data.games[j];  
@@ -66,6 +81,53 @@ function gamesController($scope, $http) {
 	};
 	$scope.numLimit = numLimit;
 	
-}
+	$scope.rating = rating;
+
+  };
+  
+  
+  
+  app.directive('starRating',
+	function() {
+		return {
+			restrict : 'A',
+			template : '<ul class="rating">'
+					 + '	<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">'
+					 + '\u2605'
+					 + '</li>'
+					 + '</ul>',
+			scope : {
+				ratingValue : '=',
+				max : '=',
+				onRatingSelected : '&'
+			},
+			link : function(scope, elem, attrs) {
+				var updateStars = function() {
+					scope.stars = [];
+					for ( var i = 0; i < scope.max; i++) {
+						scope.stars.push({
+							filled : i < scope.ratingValue
+						});
+					}
+				};
+				
+				scope.toggle = function(index) {
+					scope.ratingValue = index + 1;
+					scope.onRatingSelected({
+						rating : index + 1
+					});
+				};
+				
+				scope.$watch('ratingValue',
+					function(oldVal, newVal) {
+						if (newVal) {
+							updateStars();
+						}
+					}
+				);
+			}
+		};
+	});
+	
 
 
